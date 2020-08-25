3f48f242887be2cb5f43cc0507cf4722b7dc1fb4
Shawn Sullivan <sullivansm3@gmail.com>
2020-08-25 10:21:54 -0600

    Add title in Head. Format date w date-fns

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,19 @@
+import Head from 'next/head'
 import Layout from '../../components/layout'
+import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
     return (
       <Layout>
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
         {postData.title}
         <br />
         {postData.id}
         <br />
-        {postData.date}
+        <Date dateString={postData.date} />
         <br />
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </Layout>
